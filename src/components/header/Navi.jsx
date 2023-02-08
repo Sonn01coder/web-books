44e649d1f3247c428309de4ec05aabc00881426f
Son Nguyen <monne2310@gmail.com>
2023-02-08 18:52:51 +0700

    Deployed website

--- src/components/header/Navi.jsx
+++ src/components/header/Navi.jsx
@@ -1,27 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Col, Container, Row, Navbar } from 'react-bootstrap';
+import { Col, Container, Row, Navbar, InputGroup, FormControl, Form } from 'react-bootstrap';
 import background1 from "../../asset/img/bg1.png"
 import "./navi.css"
 
 
 function Navi(props) {
     return (
-        <Container class="fixed-top navi_container">
-            <Navbar >
-                <Col md="8"></Col>
-                <Col md="2">Dang ki</Col>
-                <Col md="2">Dang nhap</Col>
+        <Container className="navi_container">
+            <Navbar>
+                <Col md={9}></Col>
+                <Col md={3}>
+                    <Row className='d-flex justify-content-end'>
+                        <Col md={2} className='login'><i class="fa-solid fa-cart-shopping"></i></Col>
+                        <Col md={6} className='login'>Đăng Nhập</Col>
+                        <Col md={4} className='login'>Đăng Kí</Col>
+                    </Row>
+                </Col>
             </Navbar>
             <Row className='navi_wrap'>
-                <Col>Một cuốn sách là một giấc mơ mà bạn cầm trong tay</Col>
-                <Col>
-                    <h1>Tìm cuốn sách của riêng bạn</h1>
+                <Col md={8} className='navi_sologan'>Một cuốn sách là một giấc mơ mà bạn cầm trong tay</Col>
+                <Col md={4} className='navi_search'>
+                    <h2>Tìm cuốn sách của riêng bạn</h2>
+                    <InputGroup className='navi_input'>
+                        <FormControl placeholder="Tìm kiếm tại đây..." />
+                        <InputGroup.Text><i class="fa-solid fa-magnifying-glass"></i></InputGroup.Text>
+                    </InputGroup>
                     <div>
-                        <input type="text" placeholder="Tìm kiếm tại đây..." />
-                        <i class="fa-solid fa-magnifying-glass"></i>
-                    </div>
-                    <div>
-                        {/* <img src={background1} alt="Background1"/> */}
+                        <img src={background1} alt="Background1" className='navi_img'/>
                     </div>
                 </Col>  
             </Row>

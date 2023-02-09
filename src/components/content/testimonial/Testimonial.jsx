b1500b80659c5bd511a11c8af0632d1365959c0c
Son Nguyen <monne2310@gmail.com>
2023-02-10 01:19:54 +0700

    Deployed website

--- src/components/content/testimonial/Testimonial.jsx
+++ src/components/content/testimonial/Testimonial.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./testimonial.css"
-import sliderImg from '../../../asset/img/slider.jpg'
+import sliderImg from '../../../asset/img/slider1.jpg'
+import { Col, Row } from 'react-bootstrap';
 
 function Testimonial(props) {
+    const [count, setCount] = useState(0)
+    const [sloganCurr, setSloganCurr] = useState(
+    [
+        "“Gặp được một quyển sách hay nên mua liền dù đọc được hay không đọc được, vì sớm muộn gì cũng cần đến nó.(W.Churchill)”",
+        "“Cần phải yêu mến và tin vào sách . Cần rèn luyện cho mình thói quen thực hành và kỹ năng dùng sách để làm việc.(N. Rubakin)”",
+        "“Bạn biết rằng bạn đã đọc một cuốn sách hay khi bạn giở đến trang cuối cùng và cảm thấy như mình vừa chia tay một người bạn.(Khuyết danh)”"
+    ])
+
+    useEffect(()=> {
+        setInterval(()=> {
+            setCount(count => count < 2 ? count + 1 : 0)
+        }, 3000)
+    }, [count])
+
     return (
         <section className='testimonial' style={{backgroundImage: `url(${sliderImg})`}}>
-            <div >
-                
-            </div>
+            <Row>
+                <Col md={2}></Col>
+                <Col md={8} className='testimonial_wrap'>
+                    <Row>
+                        <Col md={1}></Col>
+                        <Col md={10} className="testimonial_text" >{sloganCurr[count]}</Col>
+                    </Row>
+
+                    <Row>
+                        <Col md={12} className="testimonial_checked" >
+                            <i class="fa-solid fa-circle testimonial_checked-active"></i>
+                            <i class="fa-solid fa-circle"></i>
+                            <i class="fa-solid fa-circle"></i>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </section>
     );
 }
